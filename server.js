2832a5cb63885a171e8b0cfd48fc43f2517aefe5
siddhantdhomse45 <siddh654@gmail.com>
2025-11-21 11:42:04 +0530

    changes in server

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ const app = express();
 // FIXED CORS (FINAL VERSION)
 // ----------------------------
 const allowedOrigins = [
-  "https://drushtieducation.vercel.app/", 
+  "https://drushtieducation.vercel.app", 
   "http://localhost:5173",
   "https://drushtieducation.com/"
 ];
